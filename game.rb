a3413755270fd4db81ad2a4b01947b697f6f526c
Charles Denahy-Knowles <cjknowles90@gmail.com>
2023-11-13 15:46:39 -0800

    Game module updated to require other classes and match userinterface inclusion.

--- game.rb
+++ game.rb
@@ -20,3 +20,13 @@ class Game
     return @player.score
   end
 end
+
+# Test cases
+player = Player.new("Alice", 0, 3)
+game = Game.new(player)
+puts "Player lives before decrease: #{player.get_lives}"
+game.decrease_lives
+puts "Player lives after decrease: #{player.get_lives}"
+puts "Player score before increase: #{player.get_score}"
+game.increase_score
+puts "Player score after increase: #{player.get_score}"
